d83672c613b950a89172a61ff3ac3f8af22f76ec
marybethnorris98 <marybeth.norris@mcgeheeschools.org>
2025-11-13 11:26:15 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,7 +13,7 @@ function setup() {
 
   // Reset button
   resetButton = createButton("🔄 Reset");
-  resetButton.position(1500, 200);
+  resetButton.position(1000, 200);
   resetButton.style('font-size', '18px');
   resetButton.mousePressed(resetShapes);
 
